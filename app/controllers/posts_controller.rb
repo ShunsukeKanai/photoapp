f1cbe859053f5636aff24cfd272c25511a967063
ShunsukeKanai <knisnsk@gmail.com>
2020-02-15 16:07:39 +0900

    新規投稿機能作成 #4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,22 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new
+    post.save
+    redirect_to posts_url, notice: "投稿を完了しました"
   end
 
   def edit
   end
+
+  private
+
+  def task_params
+    params.require(:post).permit(:description)
+  end
+
 end
