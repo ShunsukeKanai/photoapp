9c601acaaf51045c99d80928651d7dd4a3ab9d0a
ShunsukeKanai <knisnsk@gmail.com>
2020-03-01 14:38:58 +0900

    userテーブルにintroduction追加 userid削除#48

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_083330) do
+ActiveRecord::Schema.define(version: 2020_03_01_052841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 2020_02_29_083330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", default: "", null: false
-    t.string "userid", default: "", null: false
     t.string "avatar"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
