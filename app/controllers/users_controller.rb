396e0373c2696f4af81a86e0b8fd1ea738a64e4f
ShunsukeKanai <knisnsk@gmail.com>
2020-03-01 18:52:05 +0900

    プロフィールの編集ページ作成#50

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    user = current_user
+    user.update!(user_params)
+    redirect_to user_path, notice: "#{user.username}さんのプロフィールを更新しました"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :introduction, :avatar)
+  end
+
 end
