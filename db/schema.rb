cc4f612b3fcf31705e39fb01f74d48b137cc32c0
ShunsukeKanai <knisnsk@gmail.com>
2020-04-13 07:59:10 +0900

    usersテーブルのusernameカラム名をnameに変更し各所のusernameをnameに変更した#107

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_145354) do
+ActiveRecord::Schema.define(version: 2020_04_12_223840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_145354) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "avatar"
     t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
