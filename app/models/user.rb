a124ae6f80ad807e02b8c0d96595451521788c8e
ShunsukeKanai <knisnsk@gmail.com>
2020-04-18 22:14:42 +0900

    user modelに検証を追加した#127

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts
   mount_uploader :avatar, AvatarUploader
 
+  validates :name, presence: true, uniqueness: { case_sensitive: true },
+                   length: { maximum: 30 }
+  validates :introduction, length: { maximum: 140 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: true }
 end
